efc351db35d9fd3eccc95915953159bce929d368
vanh <vanhphom@gmail.com>
2020-07-18 02:53:29 -0700

    UI test with espresso

--- app/src/androidTest/java/com/vanh/android/architecture/blueprints/whattodoapp/taskdetail/TaskDetailFragmentTest.kt
+++ app/src/androidTest/java/com/vanh/android/architecture/blueprints/whattodoapp/taskdetail/TaskDetailFragmentTest.kt
@@ -2,6 +2,9 @@ package com.vanh.android.architecture.blueprints.whattodoapp.taskdetail
 
 //import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.vanh.android.architecture.blueprints.whattodoapp.R
@@ -11,6 +14,7 @@ import com.vanh.android.architecture.blueprints.whattodoapp.data.Task
 import com.vanh.android.architecture.blueprints.whattodoapp.data.source.TasksRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers.not
 import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
@@ -35,15 +39,34 @@ class TaskDetailFragmentTest {
     }
 
     @Test
-    fun activeTaskDetails_DisplayedInUi() = runBlockingTest {
+    fun activeTaskDetails_DisplayedInUi() = runBlockingTest{
         // GIVEN - Add active (incomplete) task to the DB
         val activeTask = Task("Active Task", "AndroidX Rocks", false)
+        repository.saveTask(activeTask)
 
         // WHEN - Details fragment launched to display task
         val bundle = TaskDetailFragmentArgs(activeTask.id).toBundle()
         launchFragmentInContainer<TaskDetailFragment>(bundle, R.style.AppTheme)
-        Thread.sleep(2000)
 
+        // THEN - Task details are displayed on the screen
+        // make sure that the title/description are both shown and correct
+        onView(withId(R.id.task_detail_title_text)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_title_text)).check(matches(withText("Active Task")))
+        onView(withId(R.id.task_detail_description_text)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_description_text)).check(matches(withText("AndroidX Rocks")))
+        // and make sure the "active" checkbox is shown unchecked
+        onView(withId(R.id.task_detail_complete_checkbox)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_complete_checkbox)).check(matches(not(isChecked())))
     }
+
+        @Test
+    fun completedTaskDetails_DisplayedInUi() = runBlockingTest{
+        // GIVEN - Add completed task to the DB
+
+        // WHEN - Details fragment launched to display task
+
+        // THEN - Task details are displayed on the screen
+        // make sure that the title/description are both shown and correct
+}
 
 }
