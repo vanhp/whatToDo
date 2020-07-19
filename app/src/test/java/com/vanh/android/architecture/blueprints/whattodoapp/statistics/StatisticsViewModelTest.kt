9a0ec4e4ae2babff6724a1ccb1783adc9964428b
vanh <vanhphom@gmail.com>
2020-07-19 16:10:08 -0700

    add pausing and resuming coroutine to inspect value while testing

--- app/src/test/java/com/vanh/android/architecture/blueprints/whattodoapp/statistics/StatisticsViewModelTest.kt
+++ app/src/test/java/com/vanh/android/architecture/blueprints/whattodoapp/statistics/StatisticsViewModelTest.kt
@@ -3,10 +3,13 @@ package com.vanh.android.architecture.blueprints.whattodoapp.statistics
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.vanh.android.architecture.blueprints.whattodoapp.data.source.FakeTestRepository
 import com.vanh.android.architecture.blueprints.whattodoapp.data.source.MainCoroutineRule
+import com.vanh.android.architecture.blueprints.whattodoapp.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.CoreMatchers.`is`
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 @ExperimentalCoroutinesApi
 class StatisticsViewModelTest {
@@ -33,4 +36,27 @@ class StatisticsViewModelTest {
 
         statisticsViewModel = StatisticsViewModel(tasksRepository)
     }
+
+    @Test
+fun loadTasks_loading() {
+        // normally the testing of coroutine usually run to completion
+        // using runBlocking api but in a case where the value of interest
+        // is occur while the coroutine is running so it need tobe paused
+    //testing coroutine need to pause the coroutine to inspect value
+    // Pause dispatcher so you can verify initial values.
+    mainCoroutineRule.pauseDispatcher()
+
+    // Load the task in the view model.
+    statisticsViewModel.refresh()
+
+    // Then progress indicator is shown.
+    assertThat(statisticsViewModel.dataLoading.getOrAwaitValue(), `is`(true))
+
+        // Execute pending coroutines actions.
+    mainCoroutineRule.resumeDispatcher()
+
+        // Then progress indicator is hidden.
+    assertThat(statisticsViewModel.dataLoading.getOrAwaitValue(), `is`(false))
+}
+
 }
