faf455abf4129ca23c7cb77dbb6c0f32eef4ce34
vanh <vanhphom@gmail.com>
2020-07-19 16:00:42 -0700

    add test for statistic viewmodel/fragment

---
+++ app/src/test/java/com/vanh/android/architecture/blueprints/whattodoapp/statistics/StatisticsViewModelTest.kt
@@ -0,0 +1,36 @@
+package com.vanh.android.architecture.blueprints.whattodoapp.statistics
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.vanh.android.architecture.blueprints.whattodoapp.data.source.FakeTestRepository
+import com.vanh.android.architecture.blueprints.whattodoapp.data.source.MainCoroutineRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+
+@ExperimentalCoroutinesApi
+class StatisticsViewModelTest {
+
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    // Set the main coroutines dispatcher for unit testing.
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    // Subject under test
+    private lateinit var statisticsViewModel: StatisticsViewModel
+
+    // Use a fake repository to be injected into the view model.
+    private lateinit var tasksRepository: FakeTestRepository
+
+    @Before
+    fun setupStatisticsViewModel() {
+        // Initialise the repository with no tasks.
+        tasksRepository = FakeTestRepository()
+
+        statisticsViewModel = StatisticsViewModel(tasksRepository)
+    }
+}
